68869c3263ccb2c11727ddde60de57d61d443b8c
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-02-15 01:21:00 +0900

    day21 Editor 페이지 완성

--- day18/src/components/Header.jsx
+++ day18/src/components/Header.jsx
@@ -3,9 +3,9 @@ import "./Header.css";
 function Header({ leftChild, title, rightChild }) {
   return (
     <header className="Header">
-      <div>{leftChild}</div>
+      <div className="header_left">{leftChild}</div>
       <div className="header_center">{title}</div>
-      <div>{rightChild}</div>
+      <div className="header_right">{rightChild}</div>
     </header>
   );
 }
